676e6a6b96432b0c4f69a7ad9a50074121379348
Richard Keil <gh@formenwandler.com>
2024-09-04 12:08:05 +0200

    fix: align cjs/esm hybrid exports

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 // For backwards compatibility, make sure require("graphql-tag") returns
 // the gql function, rather than an exports object.
-module.exports = require('./lib/graphql-tag.umd.js').gql;
+const gql = require('./lib/graphql-tag.umd.js').gql
+module.exports = gql.gql = gql
